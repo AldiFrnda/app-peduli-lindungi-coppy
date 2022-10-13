4f6a0f8ea9756a5a06ed9cea44c24751262861a7
AldiFrnda <115371805+AldiFrnda@users.noreply.github.com>
2022-10-13 19:48:21 +0700

    upgrade

--- App/Routes.js
+++ App/Routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
-  HomeScreen, 
-  SplashScreen, 
+  SplashScreen,
   LoginScreen,
-  RegisterScreen,
-  RegisterSuccessScreen,
-  RegisterErrorScreen
+  HomeScreen,
+  ImmunScreen,
+  EhacScreen,
+  CovidTestScreen
 } from './Pages';
 
 const Stack = createStackNavigator();
@@ -14,12 +14,12 @@ const Stack = createStackNavigator();
 export default Router = () => {
   return (
     <Stack.Navigator initialRouteName="SplashScreen">
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false}} />
       <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown: false}} />
-      <Stack.Screen name="RegisterSuccessScreen" component={RegisterSuccessScreen} options={{headerShown: false}} />
-      <Stack.Screen name="RegisterErrorScreen" component={RegisterErrorScreen} options={{headerShown: false}} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
+      <Stack.Screen name="ImmunScreen" component={ImmunScreen} options={{headerShown: false}} />
+      <Stack.Screen name="EhacScreen" component={EhacScreen} options={{headerShown: false}} />
+      <Stack.Screen name="CovidTestScreen" component={CovidTestScreen} options={{headerShown: false}} />
     </Stack.Navigator>
   );
 };
